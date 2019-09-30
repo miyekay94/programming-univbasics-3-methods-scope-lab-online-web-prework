820059e53d03b61214bdcbbf3907e914275593e4
Mary Kanu <maryikanu@gmail.com>
2019-09-30 03:47:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,6 +9,12 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  
+  describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
+  
 end
 
 def toadstool
