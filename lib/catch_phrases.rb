26b24b0364f7f775567f90bf27a3dd1376aa2239
Mary Kanu <maryikanu@gmail.com>
2019-09-30 03:48:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -19,4 +19,9 @@ end
 
 def toadstool
   puts status
+  
+  describe "toadstool" do
+  it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
+    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
+  end
 end
