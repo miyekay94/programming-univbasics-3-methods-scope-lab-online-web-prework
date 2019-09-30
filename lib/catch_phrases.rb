585e5de051e3def1490ae5b330039e6604124589
Mary Kanu <maryikanu@gmail.com>
2019-09-30 03:46:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,11 @@
+# Method Scope Lab
+
+## Learning Goals
+
+- Define a local variable
+- Define a method
+- Define a method that takes an argument
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
